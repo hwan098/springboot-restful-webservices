80633d6df2ebd26c79a612688ef3cc3ec6a55d2f
Choi Changhwan <glqkfl7845@gmail.com>
2023-08-05 18:58:17 +0900

    Get All Users REST API
     UserService 인터페이스에서 getAllUsers 메서드를 정의한 후 UserServiceImpl 클래스에서 getAllUsers 메서드를 구현. 
    
     UserController에서 모든 User 개체를 검색하고 검색된 개체를 상태코드와 함께 변환하는 getAllUsers 메서드 생성.
    
     postman에서 상태코드 및  검색이 잘 되는지 확인하였음.

--- src/main/java/net/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
@@ -3,6 +3,8 @@ package net.javaguides.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.run;
+
 @SpringBootApplication
 public class SpringbootRestfulWebservicesApplication {
 
