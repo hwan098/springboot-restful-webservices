b1ca8ceaef8d48b7e8e7eac51da22f40f5ca21c6
Choi Changhwan <glqkfl7845@gmail.com>
2023-08-05 18:09:00 +0900

    Create User REST API
    들어오는 HTTP 요청(JSON)을 받아 JAVA 객체로 변환하고 users 테이블에 저장할 수 있게 함. postman, DBeaber에서 상태코드 및  테이블에 잘 생성이 되었는지 확인하였음

---
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -0,0 +1,23 @@
+package net.javaguides.springboot.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.entity.User;
+import net.javaguides.springboot.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/users")
+public class UserController {
+
+    private UserService userService;
+
+    // build create User REST API
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User saveUser = userService.createUser(user);
+        return new ResponseEntity<>(saveUser, HttpStatus.CREATED);
+    }
+}
